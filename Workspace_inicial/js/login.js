0e8a4b0b056ae7189cf41e39a7ed10f24d66fe86
juanmescobar <68871719+juanmescobar@users.noreply.github.com>
2020-08-16 17:57:16 -0300

    Pagina de login
    Se añadió una pagina de login

--- Workspace_inicial/js/login.js
+++ Workspace_inicial/js/login.js
@@ -1,6 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
 });
+
+//función para redireccionar
+function redirect(){
+    location.href="https://juanmescobar.github.io/Jap-github/Workspace_inicial/main/"
+}
+const loginForm = document.getElementById("login-form");
+
+// verificación del form antes de redireccionar
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username.includes(@)  && 8<(length(password))<16 {
+        redirect();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
